0ad0cfa8d80e35d88d0e01d5274de9c0275c4dda
Kamil <baldygakamil@gmail.com>
2025-03-11 09:50:17 +0100

    finished adding options to const

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,6 @@ Logic of the program:
  -  Stock 10 questions in an array
  -  Score of 10 system
  -  5 countries
- -  Randomize questions
 */
 
 const question1 = "Quelle est la capitale de la Belgique?";
@@ -13,30 +12,33 @@ const question2 = "Quel monument emblématique se trouve à Paris, en France?";
 const options2 = "Big Ben, La Tour Eiffel, La Sagrada Familia, Le Colisée";
 
 const question3 = "Quelle est la capitale de la Pologne?";
-const options3 = "Prague, Cracovie, Varsovie, Vienne"
+const options3 = "Prague, Cracovie, Varsovie, Vienne";
 
 const question4 = "Helsinki est la capitale de quel pays?";
-// options: "Tchéquie", "Finlande", "Turquie", "Suède"
+const options4 = "Tchéquie, Finlande, Turquie, Suède";
 
 const question5 = "Quel pays a pour symbole le coq gaulois?";
-// options: "Belgique", "Pologne", "France", "Turquie"
+const options5 = "Belgique, Pologne, France, Turquie";
 
 const question6 = "Quelle est la spécialité culinaire belge?";
-// options: "La pizza", "Les sushis", "Les frites", "Le kebab"
+const options6 = "La pizza, Les sushis, Les frites, Le kebab";
 
 const question7 = "Quelle mer borde la Pologne au nord?";
-// options: "La mer Méditerranée", "La mer Noire", "La mer Baltique", "La mer du Nord"
+const options7 =
+  "La mer Méditerranée, La mer Noire, La mer Baltique, La mer du Nord";
 
 const question8 =
   "Quel détroit sépare l'Europe de l'Asie et traverse Istanbul?";
-// options: "Le détroit de Gibraltar", "Le détroit du Bosphore", "Le détroit de Messine", "Le détroit de Béring"
+const options8 =
+  "Le détroit de Gibraltar, Le détroit du Bosphore, Le détroit de Messine, Le détroit de Béring";
 
 const question9 = "Quelle mer borde les côtes de la Turquie?";
-// options: "La mer Méditerranée", "La mer Baltique", "La mer du Nord", "La mer Caspienne"
+const options9 =
+  "La mer Méditerranée, La mer Baltique, La mer du Nord, La mer Caspienne";
 
 const question10 =
   "Lequel de ces pays est connu pour ses nombreux lacs et saunas?";
-// options: "France", "Belgique", "Turquie", "Finlande"
+const options10 = "France, Belgique, Turquie, Finlande";
 
 // Code
 alert("Bienvenue dans le Quizz sur les Pays!");
