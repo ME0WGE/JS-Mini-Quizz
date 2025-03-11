1a7021c05e72eff16d43925e88c41d23da8a51cb
Kamil <baldygakamil@gmail.com>
2025-03-11 09:29:30 +0100

    restructure question logic

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,13 @@ Logic of the program:
  -  Randomize questions
 */
 
-const pays = ["Belgique", "France", "Pologne", "Italie", "Turquie"];
-const questions = ["", "", "", "", "", "", "", "", "", ""];
-
+const question1 = "Quelle est la capitale de la Belgique?"
+const question2 = "Dans quel pays se trouve la Tour Eiffel?"
+const question3 = "Quelle est la capitale de la Pologne ?"
+const question4 = ""
+const question5 = ""
+const question6 = ""
+const question7 = ""
+const question8 = ""
+const question9 = ""
+const question10 = ""
