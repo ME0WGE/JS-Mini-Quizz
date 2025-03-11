869ed9901d76344475f230d37398187e03ac4804
Kamil <baldygakamil@gmail.com>
2025-03-11 09:24:35 +0100

    add arrays with countries

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+/*
+Logic of the program:
+ -  Stock 10 questions in an array
+ -  Score of 10 system
+ -  5 countries
+ -  Randomize questions
+*/
+
+const pays = ["Belgique", "France", "Pologne", "Italie", "Turquie"];
+const questions = ["", "", "", "", "", "", "", "", "", ""];
+
