a98ceaf3452289179090c5665cc9c0c0dde48873
Kamil <baldygakamil@gmail.com>
2025-03-13 09:45:17 +0100

    reworked option consts

--- public/js/main.js
+++ public/js/main.js
@@ -2,36 +2,52 @@ const question1 = "Quelle est la capitale de la Belgique?";
 const options1 = ["Paris", "Bruxelles", "Amsterdam", "Berlin"];
 
 const question2 = "Quel monument emblématique se trouve à Paris, en France?";
-const options2 = "Big Ben, La Tour Eiffel, La Sagrada Familia, Le Colisée";
-
+const options2 = [
+  "Big Ben",
+  "La Tour Eiffel",
+  "La Sagrada Familia",
+  "Le Colisée",
+];
 const question3 = "Quelle est la capitale de la Pologne?";
-const options3 = "Prague, Cracovie, Varsovie, Vienne";
+const options3 = ["Prague", "Cracovie", "Varsovie", "Vienne"];
 
 const question4 = "Helsinki est la capitale de quel pays?";
-const options4 = "Tchéquie, Finlande, Turquie, Suède";
+const options4 = ["Tchéquie", "Finlande", "Turquie", "Suède"];
 
 const question5 = "Quel pays a pour symbole le coq gaulois?";
-const options5 = "Belgique, Pologne, France, Turquie";
+const options5 = ["Belgique", "Pologne", "France", "Turquie"];
 
 const question6 = "Quelle est la spécialité culinaire belge?";
-const options6 = "La pizza, Les sushis, Les frites, Le kebab";
+const options6 = ["La pizza", "Les sushis", "Les frites", "Le kebab"];
 
 const question7 = "Quelle mer borde la Pologne au nord?";
-const options7 =
-  "La mer Méditerranée, La mer Noire, La mer Baltique, La mer du Nord";
+const options7 = [
+  "La mer Méditerranée",
+  "La mer Noire",
+  "La mer Baltique",
+  "La mer du Nord",
+];
 
 const question8 =
   "Quel détroit sépare l'Europe de l'Asie et traverse Istanbul?";
-const options8 =
-  "Le détroit de Gibraltar, Le détroit du Bosphore, Le détroit de Messine, Le détroit de Béring";
+const options8 = [
+  "Le détroit de Gibraltar",
+  "Le détroit du Bosphore",
+  "Le détroit de Messine",
+  "Le détroit de Béring",
+];
 
 const question9 = "Quelle mer borde les côtes de la Turquie?";
-const options9 =
-  "La mer Méditerranée, La mer Baltique, La mer du Nord, La mer Caspienne";
+const options9 = [
+  "La mer Méditerranée",
+  "La mer Baltique",
+  "La mer du Nord",
+  "La mer Caspienne",
+];
 
 const question10 =
   "Lequel de ces pays est connu pour ses nombreux lacs et saunas?";
-const options10 = "France, Belgique, Turquie, Finlande";
+const options10 = ["France", "Belgique", "Turquie", "Finlande"];
 
 //-----------------------------------------------------------------//
 // Quizz variables
@@ -45,7 +61,7 @@ let btn4 = document.querySelector(".btn4");
 let btn5 = document.querySelector(".btn5");
 let btn6 = document.querySelector(".btn6");
 let add_btn = document.createElement("button");
-let quesionCount = document.querySelector(".question-count");
+let questionCount = document.querySelector(".question-count");
 
 // Score variables
 let scoreCounter = document.querySelector(".score-counter");
@@ -71,30 +87,30 @@ function btnResponse(optionNumber) {
   btn5.innerHTML = `Indice`;
 }
 
-function btnGagnant(btn1, btn2, btn3, btn4) {
-  btn1.addEventListener("click", () => {
+function btnGagnant(btnGagnant, btn2, btn3, btn4) {
+  btnGagnant.addEventListener("click", () => {
     result.innerHTML = "BRAVOOO!";
     result.hidden = false;
 
-    btn1.remove();
-    btn2.remove();
-    btn3.remove();
-    btn4.remove();
-    btn5.remove();
+    btnGagnant.hidden = true;
+    btn2.hidden = true;
+    btn3.hidden = true;
+    btn4.hidden = true;
+    btn5.hidden = true;
     btn6.hidden = false;
   });
 }
 
-function btnPerdant(btn1, btn2, btn3, btn4) {
-  btn1.addEventListener("click", () => {
+function btnPerdant(btnPerdant, btn2, btn3, btn4) {
+  btnPerdant.addEventListener("click", () => {
     result.innerHTML = "NUUUUUL!";
     result.hidden = false;
 
-    btn1.remove();
-    btn2.remove();
-    btn3.remove();
-    btn4.remove();
-    btn5.remove();
+    btnPerdant.hidden = true;
+    btn2.hidden = true;
+    btn3.hidden = true;
+    btn4.hidden = true;
+    btn5.hidden = true;
     btn6.hidden = false;
   });
 }
@@ -104,16 +120,40 @@ function btnPerdant(btn1, btn2, btn3, btn4) {
 // let userInput = prompt("Saisissez votre nom d'utilisateur");
 // title.innerHTML = `Bienvenue, ${userInput}`;
 scoreCounter.innerHTML = score;
+questionCount.innerHTML = "Question 1";
 result.hidden = true;
 btn6.hidden = true;
+
 // Question 1
-question.innerHTML = `Question 1: ${question1}`;
+question.innerHTML = `${question1}`;
 btnResponse(options1);
 btnIndice(btn1, btn4, btn2, btn3);
 
 if (btnGagnant(btn2, btn1, btn3, btn4)) {
-  score = score + 1;
 } else if (btnPerdant(btn1, btn2, btn3, btn4)) {
 } else if (btnPerdant(btn3, btn4, btn2, btn1)) {
 } else if (btnPerdant(btn4, btn3, btn2, btn1)) {
 }
+
+btn6.addEventListener("click", () => {
+  // Question 2
+  question.innerHTML = `${question2}`;
+  btnResponse(options2);
+  btnIndice(btn1, btn4, btn2, btn3);
+
+  btn1.hidden = false;
+  btn2.hidden = false;
+  btn3.hidden = false;
+  btn4.hidden = false;
+  btn5.hidden = false;
+
+  questionCount.innerHTML = "Question 2";
+  result.hidden = true;
+  btn6.hidden = true;
+
+  if (btnGagnant(btn2, btn1, btn3, btn4)) {
+  } else if (btnPerdant(btn1, btn2, btn3, btn4)) {
+  } else if (btnPerdant(btn3, btn4, btn2, btn1)) {
+  } else if (btnPerdant(btn4, btn3, btn2, btn1)) {
+  }
+});
