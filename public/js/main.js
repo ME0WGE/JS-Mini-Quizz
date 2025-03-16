61ed5ecd6f53025539c637e36d6912ecca386e95
Kamil <baldygakamil@gmail.com>
2025-03-16 20:20:06 +0100

    remove btnIndice

--- public/js/main.js
+++ public/js/main.js
@@ -59,7 +59,6 @@ let btn1 = document.querySelector(".btn1");
 let btn2 = document.querySelector(".btn2");
 let btn3 = document.querySelector(".btn3");
 let btn4 = document.querySelector(".btn4");
-let btn5 = document.querySelector(".btn5");
 let btn6 = document.querySelector(".btn6");
 let btn7 = document.querySelector(".btn7");
 let btn8 = document.querySelector(".btn8");
@@ -79,23 +78,11 @@ let score = 0;
 
 //-----------------------------------------------------------------//
 // Functions
-function btnIndice(btnSecondary1, btnSecondary2, btnWarning1, btnWarning2) {
-  btn5.addEventListener("click", () => {
-    btnSecondary1.classList.add("btn-secondary");
-    btnSecondary2.classList.add("btn-secondary");
-    btnWarning1.classList.add("btn-warning");
-    btnWarning2.classList.add("btn-warning");
-    btn5.remove();
-  });
-}
-
 function resetButtons() {
   btn1.className = "btn btn1";
   btn2.className = "btn btn2";
   btn3.className = "btn btn3";
   btn4.className = "btn btn4";
-
-  btn5.hidden = false;
 }
 
 function btnResponse(optionNumber) {
@@ -103,7 +90,6 @@ function btnResponse(optionNumber) {
   btn2.innerHTML = `${optionNumber[1]}`;
   btn3.innerHTML = `${optionNumber[2]}`;
   btn4.innerHTML = `${optionNumber[3]}`;
-  btn5.innerHTML = `Indice`;
 }
 
 function btnGagnant(btnGagnant, btn2, btn3, btn4, btnContinuer) {
@@ -118,7 +104,6 @@ function btnGagnant(btnGagnant, btn2, btn3, btn4, btnContinuer) {
     btn2.hidden = true;
     btn3.hidden = true;
     btn4.hidden = true;
-    btn5.hidden = true;
 
     btnContinuer.hidden = false;
   });
@@ -133,7 +118,6 @@ function btnPerdant(btnPerdant, btn2, btn3, btn4, btnContinuer) {
     btn2.hidden = true;
     btn3.hidden = true;
     btn4.hidden = true;
-    btn5.hidden = true;
 
     btnContinuer.hidden = false;
   });
@@ -202,60 +186,50 @@ btn15.hidden = true;
 scoreCounter.innerHTML = `${score}`;
 
 // Question 1
-btnIndice(btn1, btn4, btn2, btn3);
 questionSelector(question1, options1, 2, 1, btn6);
 resetButtons();
 // Question 2
 btn6.addEventListener("click", () => {
-  // btnIndice(btn1, btn4, btn2, btn3);
   questionSelector(question2, options2, 2, 2, btn7);
   btn6.remove();
 });
 // Question 3
 btn7.addEventListener("click", () => {
-  // btnIndice(btn1, btn3, btn2, btn4);
   questionSelector(question3, options3, 3, 3, btn8);
   btn7.remove();
 });
 // Question 4
 btn8.addEventListener("click", () => {
-  // btnIndice(btn1, btn3, btn2, btn4);
   questionSelector(question4, options4, 2, 4, btn9);
   btn8.remove();
 });
 // Question 5
 btn9.addEventListener("click", () => {
-  // btnIndice(btn1, btn3, btn2, btn4);
   questionSelector(question5, options5, 3, 5, btn10);
   btn9.remove();
 });
 // Question 6
 btn10.addEventListener("click", () => {
-  // btnIndice(btn1, btn3, btn2, btn4);
   questionSelector(question6, options6, 3, 6, btn11);
   btn10.remove();
 });
 // Question 7
 btn11.addEventListener("click", () => {
-  // btnIndice(btn1, btn3, btn2, btn4);
   questionSelector(question7, options7, 3, 7, btn12);
   btn11.remove();
 });
 // Question 8
 btn12.addEventListener("click", () => {
-  // btnIndice(btn1, btn3, btn2, btn4);
   questionSelector(question8, options8, 2, 8, btn13);
   btn12.remove();
 });
 // Question 9
 btn13.addEventListener("click", () => {
-  // btnIndice(btn1, btn3, btn2, btn4);
   questionSelector(question9, options9, 1, 9, btn14);
   btn13.remove();
 });
 // Question 10
 btn14.addEventListener("click", () => {
-  // btnIndice(btn1, btn3, btn2, btn4);
   questionSelector(question10, options10, 4, 10, null);
   btn14.remove();
 });
